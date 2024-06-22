751894d6db4f53c58371fcd2dd6cfd0ef20ab389
rafael <rrrr0ffff@gmail.com>
2024-06-22 19:46:17 -0300

    added post route

--- internal/api.go
+++ internal/api.go
@@ -1,17 +1,27 @@
 package internal
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 )
 
 type APIServer struct {
 	addr string
+	db   []User
+}
+
+type User struct {
+	Id   int    `json:"id"`
+	Name string `json:"name"`
 }
 
 func NewAPIServer(addr string) *APIServer {
+	users := []User{}
 	return &APIServer{
 		addr: addr,
+		db:   users,
 	}
 }
 
@@ -21,6 +31,15 @@ func (s *APIServer) Run() error {
 		userID := r.PathValue("userID")
 		w.Write([]byte("User ID: " + userID))
 	})
+	router.HandleFunc(
+		"POST /users",
+		func(w http.ResponseWriter, r *http.Request) {
+			var user User
+			json.NewDecoder(r.Body).Decode(&user)
+			s.db = append(s.db, user)
+			fmt.Println(s.db)
+		},
+	)
 
 	server := http.Server{
 		Addr:    s.addr,
